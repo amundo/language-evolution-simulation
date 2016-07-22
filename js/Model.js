04c673ff6cf60ce70ed4af455b1b672451035171
Patrick Hall <pjh@umail.ucsb.edu>
2016-07-22 15:16:21 -0700

    convert Model to es6 class

--- js/Model.js
+++ js/Model.js
@@ -1,40 +1,45 @@
-function Model(size) {
-  this.width = 51;
-  this.height = 42;
-  this.iteration = -1;
-  this.islands = [];
-  this.agents = [];
-  this.eventLog = new EventLog();
-  this.counter = new Counter();
+class Model { 
+  constructor(size) {
+    this.width = 51;
+    this.height = 42;
+    this.iteration = -1;
+    this.islands = [];
+    this.agents = [];
+    this.eventLog = new EventLog();
+    this.counter = new Counter();
 
-  window.__COUNTER__ = this.counter;
+    window.__COUNTER__ = this.counter;
+  }
+
+  setup(){
+    Object.keys(__ISLANDS__).forEach(islandKey => {
+      this.islands[islandKey] = new Island(islandKey, this);
+      for (var i = 0; i < __POPULATION__[islandKey]; i++) {
+        this.agents.push(
+          new Agent(
+            this.islands[islandKey],
+            this,
+            this.eventLog,
+            this.counter
+          )
+        );
+      }
+    })
+  
+    this.agents.forEach(agent => {
+      console.log(agent.island.code);
+      agent.setup()
+    });
+  };
+
+  step() {
+    this.iteration++;
+  
+    this.agents.forEach(function (agent) {
+      agent.step();
+    });
+  
+  };
+
 }
 
-Model.prototype.setup = function () {
-  Object.keys(__ISLANDS__).forEach(function (islandKey) {
-    this.islands[islandKey] = new Island(islandKey, this);
-    for (var i = 0; i < __POPULATION__[islandKey]; i++) {
-      this.agents.push(
-        new Agent(
-          this.islands[islandKey],
-          this,
-          this.eventLog,
-          this.counter
-        )
-      );
-    }
-  }.bind(this));
-
-  this.agents.forEach(function (agent) {
-    agent.setup();
-  });
-};
-
-Model.prototype.step = function () {
-  this.iteration++;
-
-  this.agents.forEach(function (agent) {
-    agent.step();
-  });
-
-};
